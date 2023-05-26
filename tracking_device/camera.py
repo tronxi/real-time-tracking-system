9b98c25a7d1d5d35ec262c417c89ec3e5d102949
tronxi <sergio.gsanchez97@gmail.com>
2023-05-26 03:43:25 +0200

    added tracking device code

---
+++ tracking_device/camera.py
@@ -0,0 +1,38 @@
+from picamera2 import Picamera2
+import subprocess
+
+
+class Camera:
+
+    def __init__(self):
+        self._picam2 = Picamera2()
+        self._width = 1280
+        self._height = 720
+        self._rtmp_url = "rtmp://tronxi.ddns.net:1935/live/test"
+        self._picam2.preview_configuration.main.size = (
+            self._width, self._height)
+        self._picam2.preview_configuration.main.format = "RGB888"
+        self._picam2.preview_configuration.align()
+        self._picam2.configure("preview")
+        self._command = ['ffmpeg',
+                         '-y',
+                         '-f', 'rawvideo',
+                         '-vcodec', 'rawvideo',
+                         '-pix_fmt', 'bgr24',
+                         '-s', "{}x{}".format(self._width, self._height),
+                         '-r', "30",
+                         '-i', '-',
+                         '-c:v', 'libx264',
+                         '-pix_fmt', 'yuv420p',
+                         '-preset', 'ultrafast',
+                         '-f', 'flv',
+                         self._rtmp_url]
+
+    def close(self):
+        self._picam2.close()
+
+    def start(self):
+        p = subprocess.Popen(self._command, stdin=subprocess.PIPE)
+        while True:
+            im = self._picam2.capture_array()
+            p.stdin.write(im.tobytes())
---
+++ tracking_device/main.py
@@ -0,0 +1,21 @@
+from tracking_device import camera
+import signal
+
+cam = camera.Camera()
+
+
+def exit_program(signum, frame):
+    cam.close()
+    exit(1)
+
+
+signal.signal(signal.SIGINT, exit_program)
+signal.signal(signal.SIGTSTP, exit_program)
+
+
+def main():
+    cam.start()
+
+
+if __name__ == "__main__":
+    main()
