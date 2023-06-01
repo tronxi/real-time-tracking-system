a5947c2cf82181479b5c7bc3cd2064d763099c11
tronxi <sergio.gsanchez97@gmail.com>
2023-06-01 03:09:03 +0200

    added @configuration in HintsConfiguration

--- dashboard_backend/src/main/java/tronxi/dashboard_backend/configuration/HintsConfiguration.java
+++ dashboard_backend/src/main/java/tronxi/dashboard_backend/configuration/HintsConfiguration.java
@@ -2,8 +2,10 @@ package tronxi.dashboard_backend.configuration;
 
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.context.annotation.Configuration;
 import tronxi.dashboard_backend.models.Event;
 
+@Configuration
 public class HintsConfiguration implements RuntimeHintsRegistrar {
     @Override
     public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
