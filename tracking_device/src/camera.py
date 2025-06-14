1eda924200165a21fd2db97214ef4de5447f2cc4
tronxi <sergio.gsanchez97@gmail.com>
2025-06-14 05:04:45 +0200

    improve camera

--- tracking_device/src/camera.py
+++ tracking_device/src/camera.py
@@ -1,34 +1,31 @@
 from picamera2 import Picamera2
 import subprocess
-import time
-import numpy as np
-import cv2
 
 class Camera:
 
     def __init__(self):
-        self._picam2 = Picamera2()
-        self._width = 1280
-        self._height = 720
+        self._width = 640     # Más pequeño = menos latencia
+        self._height = 480
+        self._framerate = 25  # Menor FPS = menos datos a transmitir
         self._rtmp_url = "rtmp://tronxi.ddns.net:1935/live/test"
 
-        config = self._picam2.create_preview_configuration(
-            main={"size": (self._width, self._height), "format": "RGB888"}
+        self._picam2 = Picamera2()
+        config = self._picam2.create_video_configuration(
+            main={"size": (self._width, self._height), "format": "YUV420"},
+            controls={"FrameDurationLimits": (int(1e6 / self._framerate), int(1e6 / self._framerate))}
         )
         self._picam2.configure(config)
 
-        self._command = [
+        self._ffmpeg_cmd = [
             'ffmpeg',
-            '-y',
             '-f', 'rawvideo',
-            '-vcodec', 'rawvideo',
-            '-pix_fmt', 'rgb24',
-            '-s', f"{self._width}x{self._height}",
-            '-r', '30',
+            '-pix_fmt', 'yuv420p',
+            '-s', f'{self._width}x{self._height}',
+            '-r', str(self._framerate),
             '-i', '-',
             '-c:v', 'libx264',
-            '-pix_fmt', 'yuv420p',
             '-preset', 'ultrafast',
+            '-tune', 'zerolatency',
             '-f', 'flv',
             self._rtmp_url
         ]
@@ -38,18 +35,16 @@ class Camera:
 
     def start(self):
         self._picam2.start()
+        ffmpeg = subprocess.Popen(self._ffmpeg_cmd, stdin=subprocess.PIPE)
         try:
-            p = subprocess.Popen(self._command, stdin=subprocess.PIPE)
             while True:
-                im = self._picam2.capture_array()
-                if p.stdin:
-                    try:
-                        p.stdin.write(im.tobytes())
-                    except BrokenPipeError:
-                        print("Error: ffmpeg")
-                        break
-                time.sleep(1 / 30.0)  # Control de FPS
+                frame = self._picam2.capture_array("main")
+                if ffmpeg.stdin:
+                    ffmpeg.stdin.write(frame.tobytes())
+        except BrokenPipeError:
+            print("FFmpeg pipe broken.")
         finally:
             self._picam2.stop()
-            p.stdin.close()
-            p.wait()
+            if ffmpeg.stdin:
+                ffmpeg.stdin.close()
+            ffmpeg.wait()
