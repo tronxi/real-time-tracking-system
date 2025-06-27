65c8699e9db29f57bbdef0cd13b0cfd9d6905e1b
tronxi <sergio.gsanchez97@gmail.com>
2025-06-28 00:46:07 +0200

    add queue for lora sender

--- tracking_device/src/lora_sender.py
+++ tracking_device/src/lora_sender.py
@@ -1,7 +1,8 @@
 import serial
 import time
 import RPi.GPIO as GPIO
-
+import threading
+from queue import Queue
 
 class LoraSender:
 
@@ -12,12 +13,26 @@ class LoraSender:
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
         GPIO.setup((self.M0, self.M1), GPIO.OUT, initial=GPIO.LOW)
+        self.queue = Queue()
+        self.running = True
 
         self.ser = serial.Serial('/dev/serial0', 9600)
+        self.sender_thread = threading.Thread(target=self._sender_loop, daemon=True)
+        self.sender_thread.start()
+
+    def _sender_loop(self):
+        while self.running:
+            body = self.queue.get()
+            try:
+                self.ser.write((body + '\n').encode())
+                time.sleep(0.3)
+            except Exception as e:
+                print("Error sending:", e)
 
     def send(self, body):
-        self.ser.write((body + '\n').encode())
-        time.sleep(0.3)
+        self.queue.put(body)
 
     def close(self):
+        self.running = False
+        self.sender_thread.join(timeout=1)
         self.ser.close()
