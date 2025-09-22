67ff05eb8572669641b366e8c9ba92e2d0774546
tronxi <sergio.gsanchez97@gmail.com>
2025-09-22 02:52:29 +0200

    add lora ground station

---
+++ tracking_device/src/lora_ground_station.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+import serial
+import time
+import os
+import rabbitmq_connection_manager
+import event as event_module
+
+PORT = '/dev/cu.usbserial-0001'
+rabbit = rabbitmq_connection_manager.RabbitmqConnectionManager()
+
+print("Checking port access...")
+if os.path.exists(PORT) and os.access(PORT, os.R_OK | os.W_OK):
+    print("Port exists and is accessible, trying to open...")
+    try:
+        ser = serial.Serial(PORT, 115200, timeout=1)
+        print("Serial port opened.")
+
+        print("Reading data (Ctrl+C to stop)...")
+        while True:
+            line = ser.readline().decode(errors='ignore').strip()
+            if line:
+                print(f"Received: {line}")
+                now = datetime.utcnow().isoformat()
+                event = event_module.Event("TM", datetime.now().isoformat(), {"raw": line})
+                rabbit.publish(event.to_json())
+            time.sleep(0.1)
+
+    except Exception as e:
+        print(f"Error opening port: {e}")
+else:
+    print(f"Port {PORT} not accessible or doesn't exist.")
